13fa405a1fdd5a3bb3ab6d864be80ac9dd3f0131
dayoung <dayoungkim9105@gmail.com>
2022-11-01 15:03:54 +0900

    feat(client): google sheet node 사용하는 것으로 교체중

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Main from "pages/Main";
-import DepositRecord from "pages/DepositRecord";
-import WithdrawRecord from "pages/WithdrawRecord";
-import Welfare from "pages/Welfare";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Main from 'pages/Main';
+import DepositRecord from 'pages/DepositRecord';
+import WithdrawRecord from 'pages/WithdrawRecord';
+import Welfare from 'pages/Welfare';
+
 const Index = () => {
+  // https://www.youtube.com/watch?v=PFJNJQCU_lo
+  const express = require('express');
+  const { google } = require('googleapis');
+
   return (
     <>
       <Routes>
-        <Route path="/welfare" element={<Welfare />} />
-        <Route path="/deposit" element={<DepositRecord />} />
-        <Route path="/withdraw" element={<WithdrawRecord />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/*" element={<Navigate replace to="/" />} />
+        <Route path='/welfare' element={<Welfare />} />
+        <Route path='/deposit' element={<DepositRecord />} />
+        <Route path='/withdraw' element={<WithdrawRecord />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/*' element={<Navigate replace to='/' />} />
       </Routes>
     </>
   );
